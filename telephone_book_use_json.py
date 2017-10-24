2aea8489f4f227da4ce8ded8a7bf399b13539350
Mykyta Pashura <rowdyswa@gmail.com>
2017-10-24 23:52:47 +0300

    make it work

--- telephone_book_use_json.py
+++ telephone_book_use_json.py
@@ -3,17 +3,19 @@ import json
 
 class UseJSON:
     def __init__(self):
-        self._json_file = json.dumps({})
-        try:
-            with open('book_in_json1.json') as f:
-                self._json_file = f.read()
-        except FileNotFoundError:
-            print('ssssss')
+        self._json_file = 'book_in_json.json'
+        # try:
+        #     with open('book_in_json1.json') as f:
+        #         self._json_file = f.read()
+        # except FileNotFoundError:
+        #     print('ssssss')
 
     def get_contacts(self):
-        return json.load(self._json_file)
+        with open(self._json_file) as f:
+            return json.load(f)
 
     def write_phone_book(self, contacts_in_json):
-        return json.dump(contacts_in_json, self._json_file, sort_keys=True, indent=4)
+        with open(self._json_file) as f:
+            return json.dump(contacts_in_json, f, sort_keys=True, indent=4)
 
 
